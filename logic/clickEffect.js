e2c76db2f131fc95ce5d5eb71dd52ca402dae172
Bozo Vuksan <bozovuksan@gmail.com>
2018-01-07 15:36:37 +0100

    Added Social Media links and not draggable div

--- logic/clickEffect.js
+++ logic/clickEffect.js
@@ -1,6 +1,6 @@
 var isReady = true;
 var counter = 0;
-$(document).bind("click touchstart",function(e){
+$(document).bind("click tap",function(e){
   counter++;
   if(isReady){
     $('body').append('<div class="circleEffect" id="circleEffect'+counter+'"></div>')
